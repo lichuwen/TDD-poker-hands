12bf3b086e4c680f534acb72c6a1363ef956cd63
lichuwen <545759585@qq.com>
2020-08-26 14:12:18 +0800

    [Karen,Alex] test:judge card state

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DemoTest {
     private static Demo demo;
@@ -17,5 +18,16 @@ public class DemoTest {
     public void test01() {
         String result = demo.run();
         assertThat(result).isEqualTo("ABC");
+    }
+
+    @Test
+    void should_return_correct_card_state_when_generate_car_type_given_2H3D5S9CKD() {
+        //given
+        String cards = "2H 3D 5S 9C KD";
+        //when
+        CardState cardState = demo.generateCarType(cards.split(" "));
+        //then
+        assertEquals(cardState.cardType, CardType.HighCard);
+        assertEquals(cardState.biggestNumber, 13);
     }
 }
