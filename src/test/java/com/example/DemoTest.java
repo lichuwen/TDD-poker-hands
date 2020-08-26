6dad978387c38faae39f18d0ba337ee2900a0d6e
lichuwen <545759585@qq.com>
2020-08-26 14:16:57 +0800

    [Karen,Alex] test:implement judge card state of Pair

--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -30,4 +30,15 @@ public class DemoTest {
         assertEquals(cardState.cardType, CardType.HighCard);
         assertEquals(cardState.biggestNumber, 13);
     }
+
+    @Test
+    void should_return_correct_card_state_when_generate_car_type_given_3A3D5S9CKD() {
+        //given
+        String cards = "3A 3D 5S 9C KD";
+        //when
+        CardState cardState = demo.generateCarType(cards.split(" "));
+        //then
+        assertEquals(cardState.cardType, CardType.Pair);
+        assertEquals(cardState.biggestNumber, 3);
+    }
 }
