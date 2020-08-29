b534cfeb86d5ede8f9b512a819fdcfc52ea051f3
lichuwen <545759585@qq.com>
2020-08-30 00:18:45 +0800

    [karen] test: add get straight_flush type test

--- test/PokerHandTest.py
+++ test/PokerHandTest.py
@@ -11,5 +11,10 @@ class PokerHandTest(unittest.TestCase):
         cardType = PokerHand.generateCardType(cards.split())
         self.assertEqual(cardType, CardType.Straight)
 
+    def test_should_return_straight_flush_type_when_generate_card_type_given_3H4H5H6H7H(self):
+        cards = "3H 4H 5H 6H 7H"
+        cardType = PokerHand.generateCardType(cards.split())
+        self.assertEqual(cardType, CardType.StraightFlush)
+
 if __name__ == '__main__':
     unittest.main()
