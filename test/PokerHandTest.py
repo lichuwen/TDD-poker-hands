f837075c25033caadb149aac899b070dbe0ff88b
lichuwen <545759585@qq.com>
2020-08-30 01:35:39 +0800

    [karen] test: add check Full House card type test

--- test/PokerHandTest.py
+++ test/PokerHandTest.py
@@ -23,10 +23,15 @@ class PokerHandTest(unittest.TestCase):
          cardType = pokerHand.generateCardType(cards.split())
          self.assertEqual(cardType, CardType.FourOfAKind)
 
-    def test_should_return_four_of_a_kind_type_when_generate_card_type_given_3H3D3D5S9C(self):
+    def test_should_return_three_of_a_kind_type_when_generate_card_type_given_3H3D3D5S9C(self):
           cards = "3H 3D 3D 5S 9C"
           cardType = pokerHand.generateCardType(cards.split())
           self.assertEqual(cardType, CardType.ThreeOfAKind)
+
+    def test_should_return_full_house_type_when_generate_card_type_given_3H3D5S5C3D(self):
+           cards = "3H 3D 5S 5C 3D"
+           cardType = pokerHand.generateCardType(cards.split())
+           self.assertEqual(cardType, CardType.FullHouse)
 
 if __name__ == '__main__':
     unittest.main()
