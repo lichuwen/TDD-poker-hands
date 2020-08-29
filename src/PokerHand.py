20f7d2e3eec5fbfa47de476466e527589cebbab5
lichuwen <545759585@qq.com>
2020-08-30 00:32:31 +0800

    [karen] refactor: extract check straight of a function

--- src/PokerHand.py
+++ src/PokerHand.py
@@ -6,14 +6,27 @@ from enum import Enum
 CardType = Enum('CardType', ('HighCard', 'Pair', 'TwoPair', 'ThreeOfAKind', 'Straight', 'Flush', 'FullHouse', 'FourOfAKind', 'StraightFlush'))
 
 class PokerHand:
+
     def __init__(self):
         pass
 
+    def isStraight(self,cards):
+        cardNum = []
+        for i in range(len(cards)):
+            cardNum.append(cards[i][0])
+        cardNum = list(map(int, cardNum))
+        min = cardNum[0]
+        for i in range(len(cardNum)):
+            min += 1
+            if (cardNum[i]+1 != min):
+                return False
+        return True
 
-    def generateCardType(cards):
-        for i in range(len(cards)-1):
-            if cards[i][1] != cards[i+1][1]:
-                return CardType.Straight
+    def generateCardType(self,cards):
+        if self.isStraight(cards):
+            for i in range(len(cards)-1):
+                if cards[i][1] != cards[i+1][1]:
+                    return CardType.Straight
         return CardType.StraightFlush
 
     
--- test/PokerHandTest.py
+++ test/PokerHandTest.py
@@ -4,16 +4,18 @@ from src.PokerHand import PokerHand,CardType
 
 import unittest
 
+pokerHand = PokerHand()
+
 class PokerHandTest(unittest.TestCase):
 
     def test_should_return_straight_type_when_generate_card_type_given_3H4D5S6C7D(self):
         cards = "3H 4D 5S 6C 7D"
-        cardType = PokerHand.generateCardType(cards.split())
+        cardType = pokerHand.generateCardType(cards.split())
         self.assertEqual(cardType, CardType.Straight)
 
     def test_should_return_straight_flush_type_when_generate_card_type_given_3H4H5H6H7H(self):
         cards = "3H 4H 5H 6H 7H"
-        cardType = PokerHand.generateCardType(cards.split())
+        cardType = pokerHand.generateCardType(cards.split())
         self.assertEqual(cardType, CardType.StraightFlush)
 
 if __name__ == '__main__':
