1f334f571d53af7faa58ad5172aa732f10086893
lichuwen <545759585@qq.com>
2020-08-30 00:19:03 +0800

    [karen] feature: implement get straight_flush type

--- src/PokerHand.py
+++ src/PokerHand.py
@@ -1,22 +1,19 @@
 import sys
-sys.path.append(r'/Users/lichuwen/BootCamp/TDD-poker-hands') # TODO: Should update your project root dir location
+sys.path.append(r'/Users/lichuwen/BootCamp/TDD-poker-hands')
 from src.CardType import CardType
 from enum import Enum
 
 CardType = Enum('CardType', ('HighCard', 'Pair', 'TwoPair', 'ThreeOfAKind', 'Straight', 'Flush', 'FullHouse', 'FourOfAKind', 'StraightFlush'))
 
 class PokerHand:
+    def __init__(self):
+        pass
+
 
     def generateCardType(cards):
-        cardNum = []
-        for i in range(len(cards)):
-            cardNum.append(cards[i][0])
-        cardNum = list(map(int, cardNum))
-        min = cardNum[0]
-        for i in range(len(cardNum)):
-            min += 1
-            if (cardNum[i]+1 != min):
-                return False
-        return CardType.Straight
+        for i in range(len(cards)-1):
+            if cards[i][1] != cards[i+1][1]:
+                return CardType.Straight
+        return CardType.StraightFlush
 
     
